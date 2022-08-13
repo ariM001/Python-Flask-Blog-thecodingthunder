7e5c63f08870c87bc8a7e572a411a47a5baa46cc
ariM001 <arijitmandal822@gmail.com>
2022-08-13 11:08:10 +0530

    upto video 6

--- video 4 - tut 2.py
+++ video 4 - tut 2.py
@@ -4,16 +4,17 @@ app = Flask(__name__)
 
 @app.route("/")    # this is one end point
 def index():
-    return render_template('index.html')
+    return render_template('demo-index.html')
 
 
 @app.route("/about")        # this is another end point
 def about():
     name = "Harry"
-    return render_template('about.html',NAME = name)
+    return render_template('demo-about.html',NAME = name)
 
 @app.route("/bootstrap")
 def bootstrap():
-    return render_template('bootstrap.html')
+    return render_template('demo-bootstrap.html')
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
