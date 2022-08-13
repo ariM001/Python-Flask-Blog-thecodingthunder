7872afdff52b38814b06e39a489a54ea232655d6
ariM001 <arijitmandal822@gmail.com>
2022-08-13 20:28:16 +0530

    upto video 9

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/codingthunder'
+db = SQLAlchemy(app)
+
+
+class Contacts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), nullable=False)
+    email = db.Column(db.String(25),  nullable=False)
+    phone_num = db.Column(db.String(12), nullable=False)
+    mes = db.Column(db.String(150),  nullable=False)
+    date = db.Column(db.String(12), nullable=True)
 
 
 @app.route("/")
@@ -17,8 +32,21 @@ def post():
     return render_template('post.html')
 
 
-@app.route("/contact")
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
+    if request.method == "POST":
+        # Add entry to the database
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone_num = request.form.get('phone_num')
+        mes = request.form.get('mes')
+
+        entry = Contacts(name=name, email=email,
+                         phone_num=phone_num, mes=mes, date=datetime.now())
+        db.session.add(entry)
+        db.session.commit()
+        return redirect('/')
+
     return render_template('contact.html')
 
 
