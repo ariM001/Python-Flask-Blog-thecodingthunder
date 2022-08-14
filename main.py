883c04a895c2380c07f28a757e793a56e39f9fa9
ariM001 <arijitmandal822@gmail.com>
2022-08-14 12:30:44 +0530

    upto video 10

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+import json
 from datetime import datetime
 
 
+with open('config.json', 'r') as c:
+    params = json.load(c)['params']
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/codingthunder'
+
+local_server = params['local_server']
+
+if local_server:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_uri']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+
 db = SQLAlchemy(app)
 
 
@@ -19,17 +30,17 @@ class Contacts(db.Model):
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html', params=params)
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', params=params)
 
 
 @app.route("/post")
 def post():
-    return render_template('post.html')
+    return render_template('post.html', params=params)
 
 
 @app.route("/contact", methods=['GET', 'POST'])
@@ -43,11 +54,12 @@ def contact():
 
         entry = Contacts(name=name, email=email,
                          phone_num=phone_num, mes=mes, date=datetime.now())
+
         db.session.add(entry)
         db.session.commit()
         return redirect('/')
 
-    return render_template('contact.html')
+    return render_template('contact.html', params=params)
 
 
 app.run(debug=True)
